9e89fcceeb5b99fd9990adcabbf30b43bb89b03c
Mozuha Shikitomi <mozuha@DESKTOP-AP0CG72.localdomain>
2022-01-27 04:13:32 +0900

    feat: complete the task...?

--- src/main/java/TimeClient.java
+++ src/main/java/TimeClient.java
@@ -17,122 +17,122 @@ import org.apache.commons.net.ntp.NtpV3Packet;
 import org.apache.commons.net.ntp.TimeInfo;
 
 public class TimeClient {
-	private static String hostUrl = "127.0.0.1";
-	private static final String NTP_SERVER = "ntp.nict.jp";
-	private static int PORT = 27780;
-	private Double minD;
-	private Double diff;  // time difference between client and server
-	private NTPRequest minNTPrequest;
-	private List<NTPRequest> ntpReqs = new ArrayList<NTPRequest>();
-	private Socket socket;
-	private OutputStream oStream;
-	private ObjectOutputStream ooStream;
-	private InputStream iStream;
-	private ObjectInputStream oiStream;
-	
+  private static String hostUrl = "127.0.0.1";
+  private static final String NTP_SERVER = "ntp.nict.jp";
+  private static int PORT = 27780;
+  private Double minD;
+  private Double diff;  // time difference between client and server
+  private NTPRequest minNTPrequest;
+  private List<NTPRequest> ntpReqs = new ArrayList<NTPRequest>();
+  private Socket socket;
+  private OutputStream oStream;
+  private ObjectOutputStream ooStream;
+  private InputStream iStream;
+  private ObjectInputStream oiStream;
+  
 
-	public TimeClient() throws ClassNotFoundException {
-		// SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+  public TimeClient() throws ClassNotFoundException {
+    // SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
     NTPUDPClient ntpClient = new NTPUDPClient();
-		Random rnd = new Random();
-		
-		try {
-			ntpClient.open();
+    Random rnd = new Random();
+    
+    try {
+      ntpClient.open();
 
-			for (int i = 0; i < 10; i++) {
-				System.out.println("Loop " + (i+1));
+      for (int i = 0; i < 10; i++) {
+        System.out.println("Loop " + (i+1));
 
-				socket = new Socket(InetAddress.getByName(hostUrl), PORT);
-				System.out.println("Connected to ServerSocket!");
-				oStream = socket.getOutputStream();
-				ooStream = new ObjectOutputStream(oStream);
-				iStream = socket.getInputStream();
-				oiStream = new ObjectInputStream(iStream);
+        socket = new Socket(InetAddress.getByName(hostUrl), PORT);
+        System.out.println("Connected to ServerSocket!");
+        oStream = socket.getOutputStream();
+        ooStream = new ObjectOutputStream(oStream);
+        iStream = socket.getInputStream();
+        oiStream = new ObjectInputStream(iStream);
 
-				InetAddress ntpServer = InetAddress.getByName(NTP_SERVER);
-				System.out.println("Connected to NTP server!\n");
-				threadSleep(rnd.nextInt(100)+10);
-				TimeInfo info = ntpClient.getTime(ntpServer);
-				minNTPrequest = new NTPRequest();
+        InetAddress ntpServer = InetAddress.getByName(NTP_SERVER);
+        System.out.println("Connected to NTP server!\n");
+        threadSleep(rnd.nextInt(100)+10);
+        TimeInfo info = ntpClient.getTime(ntpServer);
+        minNTPrequest = new NTPRequest();
 
-				info.computeDetails();
-				// Date exactTime = new Date(System.currentTimeMillis() + info.getOffset());
-				// System.out.println("Correct time:\n" + formatter.format(exactTime) + "\n");
+        info.computeDetails();
+        // Date exactTime = new Date(System.currentTimeMillis() + info.getOffset());
+        // System.out.println("Correct time:\n" + formatter.format(exactTime) + "\n");
 
-				NtpV3Packet packet = info.getMessage();
+        NtpV3Packet packet = info.getMessage();
 
-				// System.out.println("[t1] (Originate TimeStamp):\n"
-				// 				+ formatter.format(packet.getOriginateTimeStamp().getDate()) + "\n");
-				minNTPrequest.setT1(packet.getOriginateTimeStamp().getTime());
+        // System.out.println("[t1] (Originate TimeStamp):\n"
+        // 				+ formatter.format(packet.getOriginateTimeStamp().getDate()) + "\n");
+        minNTPrequest.setT1(packet.getOriginateTimeStamp().getTime());
 
-				// System.out.println("[t2] (Receive TimeStamp):\n"
-				// 				+ formatter.format(packet.getReceiveTimeStamp().getDate()) + "\n");
-				//minNTPrequest.setT2(packet.getReceiveTimeStamp().getTime());
+        // System.out.println("[t2] (Receive TimeStamp):\n"
+        // 				+ formatter.format(packet.getReceiveTimeStamp().getDate()) + "\n");
+        //minNTPrequest.setT2(packet.getReceiveTimeStamp().getTime());
 
-				// System.out.println("[t3] (Transmit TimeStamp):\n"
-				// 				+ formatter.format(packet.getTransmitTimeStamp().getDate()) + "\n");
-				//minNTPrequest.setT3(packet.getTransmitTimeStamp().getTime());
+        // System.out.println("[t3] (Transmit TimeStamp):\n"
+        // 				+ formatter.format(packet.getTransmitTimeStamp().getDate()) + "\n");
+        //minNTPrequest.setT3(packet.getTransmitTimeStamp().getTime());
 
-				// System.out.println("[t4] (Return TimeStamp):\n"
-				// 				+ formatter.format(new Date(info.getReturnTime())) + "\n");
-				minNTPrequest.setT4(info.getReturnTime());
+        // System.out.println("[t4] (Return TimeStamp):\n"
+        // 				+ formatter.format(new Date(info.getReturnTime())) + "\n");
+        minNTPrequest.setT4(info.getReturnTime());
 
-				// System.out.println("Roundtrip Time:\n" + info.getDelay() + "ms\n");
-				// System.out.println("Offset:\n" + info.getOffset() + "ms\n");
+        // System.out.println("Roundtrip Time:\n" + info.getDelay() + "ms\n");
+        // System.out.println("Offset:\n" + info.getOffset() + "ms\n");
 
-				sendNTPRequest(minNTPrequest);
-				minNTPrequest = (NTPRequest) oiStream.readObject();
+        sendNTPRequest(minNTPrequest);
+        minNTPrequest = (NTPRequest) oiStream.readObject();
 
-				minNTPrequest.calculateOandD();
-				System.out.println("Delay:\n" + minNTPrequest.d + "ms\n");
-				System.out.println("Offset:\n" + minNTPrequest.o + "ms\n");
+        minNTPrequest.calculateOandD();
+        System.out.println("Delay:\n" + minNTPrequest.d + "ms\n");
+        System.out.println("Offset:\n" + minNTPrequest.o + "ms\n");
 
-				ntpReqs.add(minNTPrequest);
+        ntpReqs.add(minNTPrequest);
 
-				// sendNTPRequest(minNTPrequest);
+        // sendNTPRequest(minNTPrequest);
 
-				//socket.close();
-				
-				threadSleep(350);
-			}
-			
-			socket.close();
-			ntpClient.close();
+        //socket.close();
+        
+        threadSleep(350);
+      }
+      
+      socket.close();
+      ntpClient.close();
 
-			minD = Double.MAX_VALUE;
-			for (NTPRequest req : ntpReqs) {
-				double tmp = minD;
-				minD = Math.min(req.d, minD);
-				if (tmp != minD) diff = req.o;  // Choose the value of 𝑜𝑖 that corresponds to the minimum 𝑑
-			}
+      minD = Double.MAX_VALUE;
+      for (NTPRequest req : ntpReqs) {
+        double tmp = minD;
+        minD = Math.min(req.d, minD);
+        if (tmp != minD) diff = req.o;  // Choose the value of 𝑜𝑖 that corresponds to the minimum 𝑑
+      }
 
-			System.out.println("Time difference between client and server is:\n" + diff + "ms\n");
-			System.out.println("Accuracy of the approximation is calculated to be:\n±" + minD/2 + "ms\n");
+      System.out.println("Time difference between client and server is:\n" + diff + "ms\n");
+      System.out.println("Accuracy of the approximation is calculated to be:\n±" + minD/2 + "ms\n");
 
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-	}
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } 
+  }
 
-	private void sendNTPRequest(NTPRequest request) throws IOException {
-		//
-		System.out.println("Sending messages to the ServerSocket");
+  private void sendNTPRequest(NTPRequest request) throws IOException {
+    //
+    System.out.println("Sending messages to the ServerSocket");
     ooStream.writeObject(request);
-		System.out.println("Sent messages to the ServerSocket\n");
-	}
+    System.out.println("Sent messages to the ServerSocket\n");
+  }
 
-	private void threadSleep(long millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+  private void threadSleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public static void main(String[] args) throws ClassNotFoundException {
-		new TimeClient();
-	}
+  public static void main(String[] args) throws ClassNotFoundException {
+    new TimeClient();
+  }
 
 }
--- src/main/java/TimeServer.java
+++ src/main/java/TimeServer.java
@@ -13,99 +13,99 @@ import org.apache.commons.net.ntp.NtpV3Packet;
 import org.apache.commons.net.ntp.TimeInfo;
 
 public class TimeServer {
-	private static int PORT = 27780;
-	private static final String NTP_SERVER = "ntp.nict.jp";
-	private ServerSocket serverSocket;
-	private NTPUDPClient ntpClient;
-	private Random rnd;
+  private static int PORT = 27780;
+  private static final String NTP_SERVER = "ntp.nict.jp";
+  private ServerSocket serverSocket;
+  private NTPUDPClient ntpClient;
+  private Random rnd;
 
-	public TimeServer() throws InterruptedException {
-		ntpClient = new NTPUDPClient();
-		rnd = new Random();
-		
-		try {
-			ntpClient.open();
-			serverSocket = new ServerSocket(PORT);
-			System.out.println("Server started on port: " + PORT);
-			//
-			while (true) {
-				Socket socket = serverSocket.accept();
-				System.out.println("Connection from " + socket + "!\n");
-				NTPRequestHandler ntpReqHandler = new NTPRequestHandler(socket);
-				ntpReqHandler.run();
-			}
+  public TimeServer() throws InterruptedException {
+    ntpClient = new NTPUDPClient();
+    rnd = new Random();
+    
+    try {
+      ntpClient.open();
+      serverSocket = new ServerSocket(PORT);
+      System.out.println("Server started on port: " + PORT);
+      //
+      while (true) {
+        Socket socket = serverSocket.accept();
+        System.out.println("Connection from " + socket + "!\n");
+        NTPRequestHandler ntpReqHandler = new NTPRequestHandler(socket);
+        ntpReqHandler.run();
+      }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			try {
-				serverSocket.close();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		}
+    } catch (IOException e) {
+      e.printStackTrace();
+      try {
+        serverSocket.close();
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+    }
 
-	}
+  }
 
-	private void threadSleep(long millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+  private void threadSleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public static void main(String[] args) throws InterruptedException {
-		new TimeServer();
-	}
+  public static void main(String[] args) throws InterruptedException {
+    new TimeServer();
+  }
 
-	private class NTPRequestHandler implements Runnable {
-		private Socket client;
-		private OutputStream oStream;
-		private ObjectOutputStream ooStream;
-		private InputStream iStream;
-		private ObjectInputStream oiStream;
+  private class NTPRequestHandler implements Runnable {
+    private Socket client;
+    private OutputStream oStream;
+    private ObjectOutputStream ooStream;
+    private InputStream iStream;
+    private ObjectInputStream oiStream;
 
-		public NTPRequestHandler(Socket client) throws InterruptedException, IOException {
-			this.client = client;
-			iStream = client.getInputStream();
-			oiStream = new ObjectInputStream(iStream);
-			oStream = client.getOutputStream();
-			ooStream = new ObjectOutputStream(oStream);
-		}
+    public NTPRequestHandler(Socket client) throws InterruptedException, IOException {
+      this.client = client;
+      iStream = client.getInputStream();
+      oiStream = new ObjectInputStream(iStream);
+      oStream = client.getOutputStream();
+      ooStream = new ObjectOutputStream(oStream);
+    }
 
-		@Override
-		public void run() {
-			///
-			try {
-				NTPRequest ntpReq = (NTPRequest) oiStream.readObject();
-				System.out.println("Received NTPRequest from: " + client);
-				// System.out.println("Client Delay: " + ntpReq.d + "ms");
-				// System.out.println("Client Offset: " + ntpReq.o + "ms");
+    @Override
+    public void run() {
+      ///
+      try {
+        NTPRequest ntpReq = (NTPRequest) oiStream.readObject();
+        System.out.println("Received NTPRequest from: " + client);
+        // System.out.println("Client Delay: " + ntpReq.d + "ms");
+        // System.out.println("Client Offset: " + ntpReq.o + "ms");
 
-				InetAddress ntpServer = InetAddress.getByName(NTP_SERVER);
-				System.out.println("Connected to NTP server!\n");
-				threadSleep(1100);
-				threadSleep(rnd.nextInt(100)+10);
-				TimeInfo info = ntpClient.getTime(ntpServer);
-				info.computeDetails();
-				NtpV3Packet packet = info.getMessage();
+        InetAddress ntpServer = InetAddress.getByName(NTP_SERVER);
+        System.out.println("Connected to NTP server!\n");
+        threadSleep(1100);
+        threadSleep(rnd.nextInt(100)+10);
+        TimeInfo info = ntpClient.getTime(ntpServer);
+        info.computeDetails();
+        NtpV3Packet packet = info.getMessage();
 
-				ntpReq.setT2(packet.getReceiveTimeStamp().getTime());
-				ntpReq.setT3(packet.getTransmitTimeStamp().getTime());
+        ntpReq.setT2(packet.getReceiveTimeStamp().getTime());
+        ntpReq.setT3(packet.getTransmitTimeStamp().getTime());
 
-				sendNTPAnswer(ntpReq);
-			} catch (ClassNotFoundException | IOException e) {
-				e.printStackTrace();
-			}
-		}
+        sendNTPAnswer(ntpReq);
+      } catch (ClassNotFoundException | IOException e) {
+        e.printStackTrace();
+      }
+    }
 
-		private void sendNTPAnswer(NTPRequest request) throws IOException {
-			///
-			System.out.println("Sending messages to the ClientSocket");
-    	ooStream.writeObject(request);
-			System.out.println("Sent messages to the ClientSocket");
-		}
+    private void sendNTPAnswer(NTPRequest request) throws IOException {
+      ///
+      System.out.println("Sending messages to the ClientSocket");
+      ooStream.writeObject(request);
+      System.out.println("Sent messages to the ClientSocket");
+    }
 
-	}
+  }
 
 }
